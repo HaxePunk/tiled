c1d24f13a8cd66fc79ef5f718e2fdfa3f23f1bd8
Matt Tuttle <heardtheword@gmail.com>
2011-05-27 22:14:10 -0500

    setCollidable function added. collisions off by default.

--- com/haxepunk/tmx/TmxEntity.hx
+++ com/haxepunk/tmx/TmxEntity.hx
@@ -8,37 +8,38 @@ import com.haxepunk.graphics.Tilemap;
 import com.haxepunk.masks.Grid;
 import flash.display.BitmapData;
 
+typedef TileFunction = Int -> Int -> Int -> Bool;
+
 class TmxEntity extends Entity
 {
 	
 	public var tilemaps:Array<Graphic>;
+	public var map:TmxMap;
 
-	public function new(map:TmxMap, bd:BitmapData, collideLayer:String = "collide", collideIndex:Int = 0) 
+	public function new(mapData:Dynamic, bitmapData:BitmapData, displayFunc:TileFunction) 
 	{
 		super();
 		var tilemap:Tilemap;
-		var layer:TmxLayer;
 		
 		tilemaps = new Array<Graphic>();
-		_grid = new Grid(map.fullWidth, map.fullHeight, map.tileWidth, map.tileHeight);
+		if (Std.is(mapData, TmxMap)) {
+			map = mapData;
+		} else {
+			map = new TmxMap(mapData);
+		}
 		
 		for (layer in map.layers)
 		{
-			tilemap = new Tilemap(bd, map.fullWidth, map.fullHeight, map.tileWidth, map.tileHeight);
+			tilemap = new Tilemap(bitmapData, map.fullWidth, map.fullHeight, map.tileWidth, map.tileHeight);
 			// Loop through tile layer ids
 			for (row in 0...layer.tileGIDs.length)
 			{
 				for (col in 0...layer.tileGIDs[row].length)
 				{
 					// if the tile is not null, set it
-					if (layer.tileGIDs[row][col] != 0)
+					if (displayFunc(layer.tileGIDs[row][col], row, col))
 					{
 						tilemap.setTile(col, row, layer.tileGIDs[row][col] - 1);
-						// if this is the collision layer, mark the tile as collidable
-						if (layer.name == collideLayer && layer.tileGIDs[row][col] > collideIndex)
-						{
-							_grid.setTile(col, row, true);
-						}
 					}
 				}
 			}
@@ -46,6 +47,36 @@ class TmxEntity extends Entity
 		}
 		
 		graphic = new Graphiclist(tilemaps);
+	}
+	
+	public function setCollidable(collideFunc:TileFunction, collideLayer:String = "collide")
+	{
+		var collide:TmxLayer = null;
+		_grid = new Grid(map.fullWidth, map.fullHeight, map.tileWidth, map.tileHeight);
+		
+		for (layer in map.layers)
+		{
+			collide = layer;
+			if (layer.name == collideLayer)
+			{
+				break;
+			}
+		}
+		
+		if (collide == null) return;
+		
+		// Loop through tile layer ids
+		for (row in 0...collide.tileGIDs.length)
+		{
+			for (col in 0...collide.tileGIDs[row].length)
+			{
+				if (collideFunc(collide.tileGIDs[row][col], row, col))
+				{
+					_grid.setTile(col, row, true);
+				}
+			}
+		}
+		
 		mask = _grid;
 		type = "solid";
 		setHitbox(_grid.width, _grid.height);

3ffe7e497df694323a0fc3f42a11dea405472b67
Matt Tuttle <heardtheword@gmail.com>
2012-05-17 08:50:41 -0500

    Fixed cross-platform issue

--- com/haxepunk/tmx/TmxTileSet.hx
+++ com/haxepunk/tmx/TmxTileSet.hx
@@ -14,7 +14,7 @@ class TmxTileSet
 {
 	private var _tileProps:Array<TmxPropertySet>;
 	private var _image:BitmapData;
-	
+
 	public var firstGID:Int;
 	public var name:String;
 	public var tileWidth:Int;
@@ -22,18 +22,18 @@ class TmxTileSet
 	public var spacing:Int;
 	public var margin:Int;
 	public var imageSource:String;
-	
+
 	//available only after immage has been assigned:
 	public var numTiles:Int;
 	public var numRows:Int;
 	public var numCols:Int;
-	
+
 	public function new(data:Dynamic)
 	{
 		var node:Fast, source:Fast;
 		numTiles = 0xFFFFFF;
 		numRows = numCols = 1;
-		
+
 		// Use the correct data format
 		if (Std.is(data, Fast))
 		{
@@ -45,37 +45,40 @@ class TmxTileSet
 			source = source.node.tileset;
 		}
 		else throw "Unknown TMX tileset format";
-		
+
 		firstGID = (source.has.firstgid) ? Std.parseInt(source.att.firstgid) : 1;
-		
+
 		// check for external source
 		if (source.has.source)
 		{
-			
+
 		}
 		else // internal
 		{
 			var node:Fast = source.node.image;
 			imageSource = node.att.source;
-			
+
 			name = source.att.name;
 			if (source.has.tilewidth) tileWidth = Std.parseInt(source.att.tilewidth);
 			if (source.has.tileheight) tileHeight = Std.parseInt(source.att.tileheight);
 			if (source.has.spacing) spacing = Std.parseInt(source.att.spacing);
 			if (source.has.margin) margin = Std.parseInt(source.att.margin);
-			
+
 			//read properties
 			_tileProps = new Array<TmxPropertySet>();
 			for (node in source.nodes.tile)
 			{
-				var id:Int = (node.has.id) ? Std.parseInt(node.att.id) : continue;
-				_tileProps[id] = new TmxPropertySet();
-				for (prop in node.nodes.properties)
-					_tileProps[id].extend(prop);
+				if (node.has.id)
+				{
+					var id:Int = Std.parseInt(node.att.id);
+					_tileProps[id] = new TmxPropertySet();
+					for (prop in node.nodes.properties)
+						_tileProps[id].extend(prop);
+				}
 			}
 		}
 	}
-	
+
 	public var image(getImage, setImage):BitmapData;
 	private function getImage():BitmapData
 	{
@@ -90,17 +93,17 @@ class TmxTileSet
 		numTiles = numRows * numCols;
 		return _image;
 	}
-	
+
 	public function hasGid(gid:Int):Bool
 	{
 		return (gid >= firstGID) && (gid < firstGID + numTiles);
 	}
-	
+
 	public function fromGid(gid:Int):Int
 	{
 		return gid - firstGID;
 	}
-	
+
 	public function toGid(id:Int):Int
 	{
 		return firstGID + id;
@@ -112,12 +115,12 @@ class TmxTileSet
 			return _tileProps[gid - firstGID];
 		return null;
 	}
-	
+
 	public function getProperties(id:Int):TmxPropertySet
 	{
 		return _tileProps[id];
 	}
-	
+
 	public function getRect(id:Int):Rectangle
 	{
 		//TODO: consider spacing & margin
